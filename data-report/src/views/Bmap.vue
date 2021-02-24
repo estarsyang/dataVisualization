7f1b5291fa519695bf913cae0ccc2c3e81b84ab4
estarsyang <455089957@qq.com>
2021-02-24 23:24:38 +0800

    feat: bmap add to project

---
+++ data-report/src/views/Bmap.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-chart :options="options"></v-chart>
+</template>
+
+<script>
+import "echarts/extension/bmap/bmap";
+const testPoint = [
+  {
+    name: "海门",
+    value: [121.15, 31.89, 80],
+  },
+  {
+    name: "南京",
+    value: [118.78, 32.04, 100],
+  },
+];
+export default {
+  data() {
+    return {
+      options: {},
+    };
+  },
+  mounted() {
+    this.options = {
+      title: {
+        text: "某某外卖大盘",
+        subtext: "大盘数据可视图",
+        left: "center",
+      },
+      bmap: {
+        key: "1XjLLEhZhQNUzd93EjU5nOGQ",
+        center: [104.114129, 37.550339],
+        zoom: 5,
+        roam: false,
+        mapStyle: {
+          styleJson: [
+            {
+              featureType: "water",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "land",
+              elementType: "all",
+              stylers: {
+                color: "#f3f3f3",
+              },
+            },
+            {
+              featureType: "railway",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "highway",
+              elementType: "all",
+              stylers: {
+                color: "#fdfdfd",
+              },
+            },
+            {
+              featureType: "highway",
+              elementType: "labels",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "geometry",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "geometry.fill",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "poi",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "green",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "subway",
+              elementType: "all",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "manmade",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "local",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "arterial",
+              elementType: "labels",
+              stylers: {
+                visibility: "off",
+              },
+            },
+            {
+              featureType: "boundary",
+              elementType: "all",
+              stylers: {
+                color: "#fefefe",
+              },
+            },
+            {
+              featureType: "building",
+              elementType: "all",
+              stylers: {
+                color: "#d1d1d1",
+              },
+            },
+            {
+              featureType: "label",
+              elementType: "labels.text.fill",
+              stylers: {
+                color: "#999999",
+              },
+            },
+          ],
+        },
+      },
+      series: [
+        {
+          name: "sale",
+          type: "scatter",
+          coordinateSystem: "bmap",
+          data: testPoint,
+        },
+      ],
+    };
+  },
+};
+</script>
