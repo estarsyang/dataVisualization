e2f394906b13883517ecee495947261c52571b8c
estarsyang <455089957@qq.com>
2021-02-25 16:28:03 +0800

    feat: add bmap demo

---
+++ data-report/src/views/Bmap2.vue
@@ -0,0 +1,25 @@
+<template>
+  <ve-map :data="options" height="800px"></ve-map>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: {
+        columns: ["位置", "税收"],
+        rows: [
+          {
+            位置: "上海",
+            税收: 123,
+          },
+          {
+            位置: "北京",
+            税收: 456,
+          },
+        ],
+      },
+    };
+  },
+};
+</script>
